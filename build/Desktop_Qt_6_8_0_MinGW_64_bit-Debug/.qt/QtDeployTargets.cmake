a5ca117c4ddb70c7fa087b56dbb8cfb14da5db75
AdamNatur <adam.natur@mail.ru>
2024-10-14 02:19:42 +0300

    init commit

---
+++ build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_ProstheticDesigner_FILE C:/Users/adamn/Documents/ProstheticDesigner/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ProstheticDesigner.exe)
+set(__QT_DEPLOY_TARGET_ProstheticDesigner_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_ProstheticDesigner_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
